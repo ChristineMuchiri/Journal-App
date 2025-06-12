47d49434b81fce38f04f22dfd3bb0bfa4d1fd716
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-12 15:40:44 +0300

    configuring amplify to application

--- my-journal/src/main.jsx
+++ my-journal/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// importing amplify and its configurations
+import { Amplify } from 'aws-amplify'
+import config from './amplifyconfiguration.json'
+
+Amplify.configure(config)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
