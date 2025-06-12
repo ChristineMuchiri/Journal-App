b3ef07f6a002356d78d1f14e1b24eb398b3463f2
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-12 15:21:58 +0300

    amplify add auth

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ a secure journal app
  npm run dev
 
  ## initialize authorization and authentication
-npm install aws-amplify
+npm install -g @aws-amplify/cl1
 amplify init //initializing amplify project
 
+## add Amazon Cognito for authenticationa and authorization
+amplify add auth
+* can customize signin/registration flow
+* customize email and SMS foor MFA
+* customize attributes for your social users
+* enable third-pary providers
+To enable federate login configure them first
+https://docs.amplify.aws/gen1/react/build-a-backend/auth/add-social-provider/#social-providers-and-federation
--- my-journal/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-journal/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,22 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myjournalApp": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "myjournalAppCustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
